de8a744d29d97bc1a80a895ff3f4cf3a02a311db
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-04 13:38:27 -0500

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,49 @@
+	 <head>
+		
+		 
+		 
+		 
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+ rel="stylesheet"
+ integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6
+ +fzT" crossorigin="anonymous">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+ 
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Homework 2 navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://mis.hw2.6.amiresta.oucreate.com/">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://mis4013.hw1.amiresta.oucreate.com">HomeWork1</a>
+      </li>
+      
+      
+    </ul>
+   
+  </div>
+</nav> 	 
+		 
+		 
+		 
+		 <title>Homework 2</title>
+  <style>
+.center {
+  margin: auto;
+  width: 60%;
+  border: 3px solid #000000;
+  padding: 10px;
+}
+	  h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+  
