ea62f46243b3f522d0d2dd663ac9972a8cce355a
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-04 13:41:14 -0500

    Update header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
  
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Homework 2 navbar</a>
+  <a class="navbar-brand" href="#">Homework 3 navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
