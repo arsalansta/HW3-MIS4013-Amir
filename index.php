863077b03bc1a8de76667164866c26cf4fe56b13
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-04 10:54:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Instructors</h1>
+    <h1>Cars</h1>
 <div class="card-group">
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "amiresta_amirsta";
+$password = "z]0qP-?ge@PG";
+$dbname = "amiresta_mydatabase1";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
