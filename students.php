dffaaaa21cb4d04f3ac4bd3fdecaa52fff74697e
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-05 13:46:43 -0500

    Update students.php

--- students.php
+++ students.php
@@ -32,7 +32,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT student_ID, Student_name from "OU Students";
+$sql = "SELECT student_ID, Student_name from "OUStudents";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
